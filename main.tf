a887cf2ffe2956f8f0daa35f376632454a6e4cb5
Karol Nedza <karol@Karols-MacBook-Pro.local>
2024-08-23 13:12:37 +0200

    spoke

--- main.tf
+++ main.tf
@@ -11,3 +11,15 @@ module "mc_transit_aws_eu_central_1" {
   enable_transit_firenet = true
   ha_gw = true
 }
+
+module "spoke_aws_1" {
+  source  = "terraform-aviatrix-modules/mc-spoke/aviatrix"
+  cloud           = "aws"
+  name            = "app1"
+  cidr            = "10.100.0.0/20"
+  region          = "eu-central-1"
+  account         = "aws-account"
+  transit_gw      = "aws-transit-eu-central-1"
+  ha_gw = false
+}
+
